fe1587ad3b22ab5c5c30a24f0a8129738d297bf9
Mathieu Chretien <MACHR47@ulaval.ca>
2024-11-20 00:54:23 -0500

    menu avancer

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -5,14 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- public void PlayGame()
+    public GameObject welcomeScreen; // Référence à l'écran "Appuyez sur une touche"
+    public GameObject Title;
+    public GameObject mainMenu;      // Référence au menu principal
+
+    private bool hasKeyPressed = false;
+
+    void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Assurez-vous que seul l'écran de bienvenue est actif au début
+        welcomeScreen.SetActive(true);
+        Title.SetActive(false);
+        mainMenu.SetActive(false);
     }
 
-    public void OptionGame()
+    void Update()
     {
+        // Détecter si une touche est pressée
+        if (!hasKeyPressed && Input.anyKeyDown)
+        {
+            Debug.Log("touche pesser");
+            hasKeyPressed = true;
+            ShowMainMenu();
+        }
+    }
 
+    void ShowMainMenu()
+    {
+        // Désactiver l'écran de bienvenue et activer le menu principal
+        welcomeScreen.SetActive(false);
+        Title.SetActive(true);
+        mainMenu.SetActive(true);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
