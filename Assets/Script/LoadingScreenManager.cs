7c94a777b1da529644d64060c69785edac526dec
Mathieu Chretien <MACHR47@ulaval.ca>
2024-11-20 13:04:41 -0500

    loading screen

---
+++ Assets/Script/LoadingScreenManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    public GameObject LoadingScreen;
+    public Image LoadingBarFill;
+   
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        LoadingScreen.SetActive(true);
+
+        while (operation != null)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress/ 0.9f);
+
+            LoadingBarFill.fillAmount = progressValue;
+           
+            yield return null;
+        }
+    }
+}
