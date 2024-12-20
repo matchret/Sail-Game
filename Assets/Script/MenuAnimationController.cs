2874171498ca4bb49025309000687ecf3824b3b3
vicou80 <102197152+vicou80@users.noreply.github.com>
2024-12-20 14:35:25 -0500

    animation menu

--- Assets/Script/MenuAnimationController.cs
+++ Assets/Script/MenuAnimationController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class MenuAnimationController : MonoBehaviour
@@ -5,30 +6,74 @@ public class MenuAnimationController : MonoBehaviour
     public UIAnimator uiAnimator;
     public RectTransform playButtonRect;
     public RectTransform optionsButtonRect;
+    public RectTransform quitButtonRect;
 
     
 
     void Start()
     {
+        //Debug.Log("MenuController Start() appelé");
+        // Le bouton arrive depuis la gauche sur une durée de 0.5s avec un offset de 500 pixels
+       // StartCoroutine(uiAnimator.AnimateInFromLeft(playButtonRect,.5f, 500f));
+       // StartCoroutine(uiAnimator.AnimateInFromLeft(optionsButtonRect,.5f, 500f));
+        //StartCoroutine(uiAnimator.AnimateInFromLeft(quitButtonRect,.5f, 500f));
+    }
+    
+    void OnEnable()
+    {
+        Debug.Log("MenuController Start() appelé");
         // Le bouton arrive depuis la gauche sur une durée de 0.5s avec un offset de 500 pixels
         StartCoroutine(uiAnimator.AnimateInFromLeft(playButtonRect,.5f, 500f));
+        StartCoroutine(uiAnimator.AnimateInFromLeft(optionsButtonRect,.5f, 500f));
+        StartCoroutine(uiAnimator.AnimateInFromLeft(quitButtonRect,.5f, 500f));
     }
+    
+
+
+
 
     public void OnPlayButtonClicked()
     {
         // Quand on clique sur Play, on fait disparaitre le panel vers le haut
-        //StartCoroutine(uiAnimator.AnimateOutUp(0.5f, 300f));
+        //StartCoroutine(uiAnimator.AnimateOutUp(playButtonRect,.5f, 300f));
     }
+    
+
+
 
     public void OnButtonHover()
     {
         // Agrandir le bouton sur 0.3s quand on le survole
-       // StartCoroutine(uiAnimator.AnimateScale(0.3f, Vector3.one, new Vector3(1.2f,1.2f,1f)));
+        StartCoroutine(uiAnimator.AnimateScale(playButtonRect,.1f, Vector3.one, new Vector3(1.5f,1.5f,1f)));
     }
 
     public void OnButtonHoverExit()
     {
         // Revenir à la taille normale
-        //StartCoroutine(playButtonAnimator.AnimateScale(0.3f, new Vector3(1.2f,1.2f,1f), Vector3.one));
+        StartCoroutine(uiAnimator.AnimateScale(playButtonRect,.1f, new Vector3(1.5f,1.5f,1f), Vector3.one));
+    }
+    
+    public void OnButtonOptionHover()
+    {
+        // Agrandir le bouton sur 0.3s quand on le survole
+        StartCoroutine(uiAnimator.AnimateScale(optionsButtonRect,.1f, Vector3.one, new Vector3(1.5f,1.5f,1f)));
+    }
+
+    public void OnButtonHoverOptionExit()
+    {
+        // Revenir à la taille normale
+        StartCoroutine(uiAnimator.AnimateScale(optionsButtonRect,.1f, new Vector3(1.5f,1.5f,1f), Vector3.one));
+    }
+    
+    public void OnButtonQuitHover()
+    {
+        // Agrandir le bouton sur 0.3s quand on le survole
+        StartCoroutine(uiAnimator.AnimateScale(quitButtonRect,.1f, Vector3.one, new Vector3(1.5f,1.5f,1f)));
+    }
+
+    public void OnButtonHoverQuitExit()
+    {
+        // Revenir à la taille normale
+        StartCoroutine(uiAnimator.AnimateScale(quitButtonRect,.1f, new Vector3(1.5f,1.5f,1f), Vector3.one));
     }
 }
